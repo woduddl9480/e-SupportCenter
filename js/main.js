917cea86becfed2aad04cfab0501b69c99421e35
SODAAAAAAA <bsohee0206@gmail.com>
2022-09-26 15:43:40 +0900

    탭 추가

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
+let lnbBtn = document.querySelectorAll('.lnb li a')
+
+function lnbSlideUp(on) {
+    let onMene = on.currentTarget.parentNode
+    onMene.classList.add('active')
+}
+function lnbSlideDown(on) {
+    let onMene = on.currentTarget.parentNode
+    onMene.classList.remove('active')
+}
+
+for(let i = 0; i < lnbBtn.length; i++) {
+    lnbBtn[i].addEventListener('mouseover', lnbSlideUp);
+}
+for(let i = 0; i < lnbBtn.length; i++) {
+    lnbBtn[i].addEventListener('mouseout', lnbSlideDown);
+}
+
 let tabBtn = document.querySelectorAll('.tab li a');
-console.log(tabBtn)
 
 function tabOn(tabClick) {
     let clicked = tabClick.currentTarget;
     let clickedLi = clicked.parentNode;
-    console.log(clicked)
-    tabClick.preventDefault();
+    let li = document.querySelectorAll('.tab li')
+
+    for(let i = 0; i < li.length; i++) {
+        li[i].classList.remove('active')
+    }
+    clickedLi.classList.add('active')
+
+    let tabIndex = Array.from(li).indexOf(clickedLi)
+    let tabcon = document.querySelectorAll('.tab-con')
+
+    for(let i = 0; i < tabcon.length; i++) {
+        tabcon[i].classList.remove('active')
+    }
+    tabcon[tabIndex].classList.add('active')
 }
 
 for(let i = 0; i < tabBtn.length; i++) {
-    tabBtn[i].addEventListener('click',tabOn);
+    tabBtn[i].addEventListener('click', tabOn);
 }
