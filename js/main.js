4128fed6dafdf59270345e207c3dce3fb5de0cf6
SODAAAAAAA <bsohee0206@gmail.com>
2022-09-26 18:20:15 +0900

    탭 수정

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 let tabBtn = document.querySelectorAll('.tab li a');
 
-tabOn((tabClick) => {
-    const clicked = tabClick.currentTarget;
+const tabOn = (tabClick) => {
+    let clicked = tabClick.currentTarget;
     const clickedLi = clicked.parentNode;
     const li = document.querySelectorAll('.tab li')
 
-    for(let i = 0; i < li.length; i++) {
+    for (let i = 0; i < li.length; i++) {
         li[i].classList.remove('active')
     }
     clickedLi.classList.add('active')
@@ -13,11 +13,11 @@ tabOn((tabClick) => {
     let tabIndex = Array.from(li).indexOf(clickedLi)
     let tabcon = document.querySelectorAll('.tab-con')
 
-    for(let i = 0; i < tabcon.length; i++) {
+    for (let i = 0; i < tabcon.length; i++) {
         tabcon[i].classList.remove('active')
     }
     tabcon[tabIndex].classList.add('active')
-});
+};
 
 for(let i = 0; i < tabBtn.length; i++) {
     tabBtn[i].addEventListener('click', tabOn);
